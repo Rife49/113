5ce1693690219a18763573f2cb08c4d42573fc1d
Robert Rife <rife4949@yahoo.com>
2026-06-11 20:08:45 -0700

    update 3

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -8,5 +8,5 @@ class SignUpView(CreateView):
     template_name = 'registration/signup.html'
     # form_class attribute allows us to create objects from a form class
     # we use this one when we want to have a custom form 
-    form_class = UserChangeForm
+    form_class = UserCreationForm
     success_url = reverse_lazy("login")
